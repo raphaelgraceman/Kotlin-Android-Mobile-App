5dbae1dfb8c7b6f31703d31cf87213c7cae8fc05
Dotse Raphael <raphaelgraceman@gmail.com>
2025-10-17 05:34:42 +0000

    Added data class

--- app/src/main/java/com/example/kotlincloudtodo/MainActivity.kt
+++ app/src/main/java/com/example/kotlincloudtodo/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlincloudtodo.ui.theme.KotlinCloudTodoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +12,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KotlinCloudTodoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                //Launch the main TodoListScreen Composable
+                TodoListScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinCloudTodoTheme {
-        Greeting("Android")
     }
 }
